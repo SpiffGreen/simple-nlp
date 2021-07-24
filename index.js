c000e200eee20211c84a8f267b6ffb75c9a1a4fd
SpiffGreen <spiffjekeygreen@gmail.com>
2021-07-24 15:04:40 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const bot = require("./training");
+console.log("Hello I'm Bujo. Let's Talk :)\n");
+bot.converse();
---
+++ simple-nlp.js
@@ -0,0 +1,110 @@
+const readline = require("readline");
+const chalk = require("chalk");
+
+const prompt = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Utilities
+function input(self, str = "> ") {
+    prompt.question(str, (value) => {
+        const result = self.process(value);
+        if(result.answer) {
+            process.stdout.write(chalk.yellow(result.answer) + "\n");
+            if(value.includes("bye")) {
+                prompt.close();
+                process.exit();
+            }
+            input(self, str);
+        } else {
+            process.stdout.write("Sorry please could you rephrase that?" + "\n");
+            input(self, str);
+        }
+    });
+    // prompt.close();
+}
+
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function flattenArray(arr) {
+    let result = [];
+    arr.forEach(i => {
+        if(!Array.isArray(i)) {
+            result.push(i);
+        } else {
+            result.push(...flattenArray(i));
+        }
+    });
+    return result;
+};
+/** Test
+const testArr = [
+    [1, 2, 3, 4],
+    5, 6, 7,
+    [8, 9, 10, [11, 12, [13, 14, 15], 16]],
+    ];
+ */
+
+/**
+ *  @description A simple example of nlp functions for building chatbots
+ *
+ */
+
+
+class Simple_Nlp {
+    constructor() {
+        this.__Markers = Object.create(null);
+        this.__Rules = Object.create(null);
+        this.__Answers = Object.create(null);
+
+        // Bind methods
+        this.addRule = this.addRule.bind(this);
+        this.addAnswer = this.addAnswer.bind(this);
+        this.train = this.train.bind(this);
+        this.process = this.process.bind(this);
+        this.converse = this.converse.bind(this);
+    }
+
+    addRule(rule, marker) {
+        this.__Markers[marker] ? this.__Markers[marker] += `|${rule}` : this.__Markers[marker] = rule;
+        if(!this.__Answers[marker]) this.__Answers[marker] = Array();
+    }
+
+    addAnswer(marker, answer) {
+        if(!this.__Answers[marker]) throw Error(`Sorry '${marker} does not exist`);
+        this.__Answers[marker].push(answer);
+    }
+
+    train() {
+        for(let marker in this.__Markers) {
+            this.__Rules[marker] = new RegExp(this.__Markers[marker], "gim");
+        }
+    }
+
+    process(str) {
+        const result = Object.create(null);
+        result.utterance = str;
+        result.possibleAnswers = Array();
+        result.classifications = Object.create(null);
+        for(let marker in this.__Rules) {
+            let match = str.match(this.__Rules[marker]);
+            if(match) {
+                result.classifications[marker] = match;
+                result.possibleAnswers.push(this.__Answers[marker])
+            }
+        }
+        result.possibleAnswers = flattenArray(result.possibleAnswers);
+        result.answer = result.possibleAnswers[randomBetween(0, result.possibleAnswers.length - 1)];
+        return result;
+    }
+
+    converse() {
+        input(this);
+    }
+}
+
+const nlp = new Simple_Nlp();
+module.exports = nlp;
